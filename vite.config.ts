a43eb9f3b26269fd3a778642e6ee3af3f3f3050b
paoloricciuti <ricciutipaolo@gmail.com>
2026-07-18 13:27:14 +0200

    chore: switch to vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vitest/config';
 import { playwright } from '@vitest/browser-playwright';
-import adapter from '@sveltejs/adapter-node';
+import adapter from '@sveltejs/adapter-vercel';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { sveltekitOG } from '@ethercorps/sveltekit-og/plugin';
 
